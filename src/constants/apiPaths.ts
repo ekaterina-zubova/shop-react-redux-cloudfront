f87e591575bc04a129477d952d725c5d0e9b2a1f
Ekaterina_Zubova <ekaterina_zubova@epam.com>
2023-03-12 21:41:21 +0300

    task 5: Integration with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://8f1uf7sjkj.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://a9lqongk8l.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
